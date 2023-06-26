dfe947697920ad63f504e3e402955679ad10fe20
Just van Rossum <justvanrossum@gmail.com>
2023-06-26 22:08:59 +0200

    Add test + test data

---
+++ tests/test_compile.py
@@ -0,0 +1,27 @@
+import pathlib
+import re
+import subprocess
+
+from fontTools.ttLib import TTFont
+
+
+ignorePat = r"<(checkSumAdjustment|modified) value=\"([^\"]+)\"/>"
+
+def cleanupTTX(ttx):
+    return re.sub(ignorePat, "------------", ttx)
+
+
+testDir = pathlib.Path(__file__).resolve().parent
+rcjkPath = testDir / "data" / "figArnaud.rcjk"
+ttxPath = testDir / "data" / "figArnaud.ttx"
+
+
+def test_main(tmpdir):
+    tmpdir = pathlib.Path(tmpdir)
+    outPath = tmpdir / "test.ttf"
+    outTTXPath = tmpdir / "test.ttx"
+    subprocess.run(["fontra-compile", rcjkPath, outPath], check=True)
+    subprocess.run(["ttx", outPath], check=True)
+    ttxLines = cleanupTTX(outTTXPath.read_text())
+    expectedLines = cleanupTTX(ttxPath.read_text())
+    assert expectedLines == ttxLines
