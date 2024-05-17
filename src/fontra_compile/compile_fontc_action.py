50fca1a59d18d6f1ad450d4d78134bbdaf3ebc97
Just van Rossum <justvanrossum@gmail.com>
2024-05-17 21:42:14 +0200

    Add compile-fontc output action

---
+++ src/fontra_compile/compile_fontc_action.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+from dataclasses import dataclass
+
+from fontra.workflow.actions import registerOutputAction
+
+from .compile_fontmake_action import CompileFontMakeAction
+
+
+@registerOutputAction("compile-fontc")
+@dataclass(kw_only=True)
+class CompileFontCAction(CompileFontMakeAction):
+
+    def compileFromDesignspace(self, designspacePath, outputFontPath, extraArguments):
+        arguments = [
+            "fontc",
+            "--output-file",
+            os.fspath(outputFontPath),
+            os.fspath(designspacePath),
+        ]
+
+        subprocess.run(arguments + extraArguments, check=True)
